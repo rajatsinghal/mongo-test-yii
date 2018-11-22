f4167d75a73a5ea178744b6759e3f1d2cd6cc9f0
Rajat Singhal <rajat0627@gmail.com>
2018-11-22 10:18:16 +0530

    convert_to_get

--- protected/controllers/EventLogController.php
+++ protected/controllers/EventLogController.php
@@ -2,8 +2,8 @@
 class EventLogController extends Controller {
 
 	public function actionIndex() {
-		if(isset($_POST['EventLog'])) {
-			$log = EventLog::create($_POST['EventLog']);
+		if(isset($_GET['EventLog'])) {
+			$log = EventLog::create($_GET['EventLog']);
 			echo CJSON::encode(['status'=>'SUCCESS', 'id'=>$log->_id]);
 		}
 	}
